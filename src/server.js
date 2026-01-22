67738b054f881f8f64b3e5b5b3ba807e85367228
Azeem Arshad Khokhar <azeem.arshad@wamolabs.com>
2026-01-22 14:09:51 +0500

    Made successfull connection to postgres database

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 import https from "https";
 import app from "./app.js";
 import fs from "fs";
+import "dotenv/config";
+import { connectToDatabase } from "./config/database.js";
 
-const port = 3000;
+const port = process.env.PORT;
 
 const options = {
   cert: fs.readFileSync("./certs/cert.pem"),
   key: fs.readFileSync("./certs/key.pem"),
 };
 
-https.createServer(options, app).listen(port, () => {
+https.createServer(options, app).listen(port, async () => {
   console.log(`Running on port: ${port}`);
+  connectToDatabase();
 });

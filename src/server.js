5e65767ab7f7514059b8bc6b49a9f55a4466040d
Azeem Arshad Khokhar <azeem.arshad@wamolabs.com>
2026-01-22 13:43:17 +0500

    Improved folder structure

--- src/server.js
+++ src/server.js
@@ -1,18 +1,13 @@
-import express from "express";
 import https from "https";
+import app from "./app.js";
 import fs from "fs";
 
-const app = express();
 const port = 3000;
 
 const options = {
   cert: fs.readFileSync("./certs/cert.pem"),
   key: fs.readFileSync("./certs/key.pem"),
 };
-
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Works" });
-});
 
 https.createServer(options, app).listen(port, () => {
   console.log(`Running on port: ${port}`);

5919e0bb81e623861eceaa789eb5e29f0f0c9e34
Azeem Arshad Khokhar <azeem.arshad@wamolabs.com>
2026-01-28 14:22:56 +0500

    Fix(Comments): Updated Associations

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,14 @@
 import User from "./User.js";
 import Post from "./Post.js";
+import Comment from "./Comment.js";
 
 User.hasMany(Post, { foreignKey: "userId", as: "posts" });
 Post.belongsTo(User, { foreignKey: "userId", as: "author" });
 
-export { User, Post };
+Post.hasMany(Comment, { foreignKey: "postId", as: "comments" });
+Comment.belongsTo(Post, { foreignKey: "postId", as: "post" });
+
+User.hasMany(Comment, { foreignKey: "userId", as: "comments" });
+Comment.belongsTo(User, { foreignKey: "userId", as: "author" });
+
+export { User, Post, Comment };
